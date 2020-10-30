a2d9f0cd77731a441aa9f620215c899b1dc3350d
Georg <georgpohl@gmx.de>
2020-10-30 16:03:29 +0000

    implemented 'enterHostnames' and 'showHostnames' with pointers

--- ganylogs.c
+++ ganylogs.c
@@ -35,76 +35,90 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#define SENTINEL 7
 
 /* CONSTANTS */
 
 /* STRUCTS */
 
 /* PROTOTYPES */
-void enterHostnames();
+void enterHostnames(char *hosts[], int *n);
+void showHostnames(char *arr[], int n);
 
 int main(int argc, const char **argv) {
-    int choice = 0;
-    // char hosts[5][9]; // noch Implementierung prüfen!!
-    printf("\n\tGANYLOGS: FILTERING TOOL FOR NEW SYSLOG MESSAGES\n");
-    printf("\t-------------------------------------------------\n\n");
+    char *routers = NULL;
+    int choice = 0, nHosts = 0;
+    // char *hosts[9]; // noch Implementierung prüfen!!
+    printf("\n\tGANYLOGS: TOOL TO IDENTIFY NEW SYSLOG MESSAGES\n");
+    printf("\t----------------------------------------------\n");
 
     do {
-        printf("\t-1- Enter hostname\n");
-        printf("\t-2- Create cronjob\n");
-        printf("\t-3- Enter syslog signature to 'blacklist'\n");
-        printf("\t-4- Enter syslog signature to 'whitelist'\n");
-        printf("\t-5- Quit\n\n");
+        putchar('\n');
+        printf("\t-1- Enter hostnames\n");
+        printf("\t-2- Add hostnames\n");
+        printf("\t-3- Show hostnames\n");
+        printf("\t-4- Create cronjob\n");
+        printf("\t-5- Enter syslog signature to 'blacklist'\n");
+        printf("\t-6- Enter syslog signature to 'whitelist'\n");
+        printf("\t-7- Quit\n\n");
 
         printf("Your choice: ");
         if ( (scanf("%d", &choice) != 1) ) {
             printf("Fehler bei der Eingabe");
             return 1;
         }
+        // putchar('\n');
 
         // Switch Anweisung
         switch (choice) {
-            case 1: enterHostnames();   // Enter hostname
+            case 1: enterHostnames(&routers, &nHosts);   // Enter hostname
             break;
             case 2: // Create cronjob
             break;
-            case 3: // Enter syslog signature to 'blacklist'
+            case 3: showHostnames(&routers, nHosts);// showHostnames(char **arr, int n) // Enter syslog signature to 'blacklist'
             break;
             case 4: // Enter syslog signature to 'whitelist'
             break;
-            case 5: printf("Bye!\n");
+            case 5: // do something
             break;
-            default:
+            case SENTINEL: printf("Bye!\n");
+            break;
+            default: printf("Gültige Auswahl 1 - %d\n\n", SENTINEL);
             break;
         }
-    }while (choice != 5);
-
+    } while (choice != SENTINEL);
+    // Free memory and avoid dangling pointers
+    
+    
   return EXIT_SUCCESS;
 }
 
-/* FUNCTIONS */
-void enterHostnames() {
-    char hosts[5][9];
-    int nHosts = 0;
+void enterHostnames(char *hosts[], int *n) {
+    *n = 0;
     printf("Enter the number of routers (<=5): ");
-    if ( (scanf("%d", &nHosts) == 0)) {
+    if ( (scanf("%d", n) == 0)) {
         printf("Input Error!\n");
         exit(1);
     }
-    for (int i=0; i < nHosts; ++i) {
-        printf("%d. Routername: ", i+1);
-        if ( (scanf("%s[^\n]", hosts[i]) == 0)) {
+    // Allocate memory for a Char-Array with 'nHosts' Elementf of size 9
+    for (int j=0; j < *n; ++j) {
+        hosts[j] = (char *)malloc(9 * sizeof(char));
+        printf("%d. Hostname: ", j+1);
+        if ( (scanf("%s[^\n]", hosts[j]) != 1)) {
             printf("Input Error!\n");
             exit(1);
-        }
+        }   
     }
+}
+
+void showHostnames(char *arr[], int n) {
     printf("\nAdded routers: ");
     printf("[");
-    for (int i=0; i < nHosts; ++i) {
+    for (int i=0; i < n; ++i) {
         if (i == 0) {
-            printf("%s", hosts[i]);
+            printf("%s", arr[i]);
         } else {
-            printf(", %s", hosts[i]);
+            printf(", %s", arr[i]);
         }
     }
     printf("]\n\n");
