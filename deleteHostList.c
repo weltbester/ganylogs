b66eae70c313d06a16a6978028439ba60d943853
Georg <georgpohl@gmx.de>
2020-11-19 17:20:25 +0000

    add deleteHostList.c

---
+++ deleteHostList.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int deleteHostList(FILE *fp, char *file) {
+    if (NULL != fp) {
+        //fclose(fp);
+        remove(file);
+        printf("Hostlist %s deleted!\n", file);
+        //fp = NULL;
+    } else {
+        printf("Nothing to delete!\n");
+    }
+    return EXIT_SUCCESS;
+}
