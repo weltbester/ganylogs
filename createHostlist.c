9bd751e5c6a273b1e3f5ef209b6dfd63a07c7970
Georg <georgpohl@gmx.de>
2020-11-20 17:17:27 +0000

    finished 'createHostlist' router counter function

--- createHostlist.c
+++ createHostlist.c
@@ -2,23 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define DS 11
+
 void createHostlist(FILE *fp) {
     char hostname[10];
-    int groesse = 0;
-
+    int groesse = 0, offset = 0;
     /* !TODO: Check if list contains already entries,
         count entries and proceed with ongoing number
-        when entering addtional routers on a second call */
-        
+        when entering addtional routers on a second call
+
+        Try to solve it with FSEEK_END and ftell and Data-size (DS)
+    */
+    fseek(fp, 0, SEEK_END);
+    offset = (int)ftell(fp) / DS;
+
     printf("How many routers to enter? ");
     if ( (scanf("%d", &groesse) != 1)) {
         printf("Input Error!\n");
         exit(EXIT_FAILURE);
     }
-    for (int i = 0; i < groesse; ++i) {
+    groesse += offset;
+    for (int i = offset; i < groesse; ++i) {
         printf("%d. Router: ", i+1);
         scanf("%s", hostname);
-        fprintf(fp, "%s\n", hostname);
+        fprintf(fp, "%-10s\n", hostname); // '%-10s' to be conform with DS
     }
     return;
 }   
